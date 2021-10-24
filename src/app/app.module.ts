b7e69b8b3981451589b9767515f2b48a80f8d4e3
Sylvester Arikhan <arikhansylvester@gmail.com>
2021-10-24 21:18:14 +0200

    Updating movie-card and app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { SynopsisCardComponent } from './synopsis-card/synopsis-card.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { RouterModule, Routes } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 const appRoutes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
@@ -49,7 +50,8 @@ const appRoutes: Routes = [
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatIconModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
